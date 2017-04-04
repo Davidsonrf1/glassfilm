c64cf8b12fb08a5ebbe77488d3a9d8608875fffb
MrChico <alvesdiego18/@gmail.com>
2017-04-04 01:28:06 -0300

    Segundo Commit

---
+++ GlassFilm/GlassFilm/FrmLogin.cs
@@ -0,0 +1,41 @@
+﻿using GlassFilm.Validacoes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GlassFilm
+{
+    public partial class FrmLogin : Form
+    {
+        public bool autorizado = false;
+
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+            Environment.Exit(1);
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            if (ValidaInternet.existeInternet())
+            {
+                autorizado = true;
+                this.Close();                
+            }
+            else
+            {
+                MessageBox.Show("Sem Internet");
+            }
+        }
+    }
+}
