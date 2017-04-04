8b356f92f1a5d23e1a9b6fafd7e63e501372b703
MrChico <alvesdiego18/@gmail.com>
2017-04-04 01:27:40 -0300

    Primeiro Commit

---
+++ GlassFilm/GlassFilm/FrmPrincipal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GlassFilm
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is MdiClient)
+                {
+                    ctrl.BackColor = ColorTranslator.FromHtml("#eaeaea");
+                }
+            }
+
+            FrmLogin frm = new FrmLogin();            
+            frm.ShowDialog();
+
+            if (frm.autorizado)
+            {
+                pnlFiltroInfo.Visible = true;
+                pnlprincipal.Visible = true;                
+            }
+        }
+    }
+}
