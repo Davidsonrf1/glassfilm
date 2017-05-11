fc2a4b2617885e344ffa571a843499c3e4e190fa
MrChico <alvesdiego18/@gmail.com>
2017-05-11 19:57:35 -0300

    Criação tela Cadastro rlo

---
+++ GlassFilm/GlassFilm/Controller/Rolo.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using GlassFilm.Class;
+using GlassFilm;
+
+namespace GlassFilm
+{
+    public class Rolo
+    {
+        private SQLiteDataAdapter da;
+        private DataTable dt;
+        private SQLiteCommand cmd;
+
+        private string Descricao { get; set; }
+        private string Largura { get; set; }
+
+        public string Id { get; set; }
+
+        public Rolo()
+        {
+            DBManager.InitDB();
+        }
+
+        public Rolo(string descricao, string largura)
+        {
+            this.Descricao = descricao;
+            this.Largura = largura;
+
+            DBManager.InitDB();
+        }
+
+        public DataTable carrega(string codigo)
+        {
+            dt = new DataTable();
+
+            try
+            {
+                string _sql = " SELECT " +
+                              "      ID," +
+                              "      DESCRICAO," +
+                              "      LARGURA" +
+                              "  FROM ROLO" +
+                              "      ";
+
+                if (codigo.Length > 0)
+                {
+                    _sql += " WHERE ID = " + codigo;
+                }
+
+                if (DBManager.conectado())
+                {
+                    da = new SQLiteDataAdapter(_sql, DBManager._mainDbName);
+                    da.Fill(dt);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logs.Log(ex.Message);
+            }
+
+            return dt;
+        }
+
+        public void excluir(string codigo)
+        {
+            if (DBManager.conectado())
+            {
+                try
+                {
+                    cmd = new SQLiteCommand();
+                    cmd.Connection = DBManager._mainConnection;
+                    cmd.CommandText = "DELETE FROM ROLO WHERE ID = @codigo";
+                    cmd.Parameters.Add(new SQLiteParameter("@codigo", codigo));
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Logs.Log(ex.Message);
+                }
+            }
+        }
+
+        public void gravar(string tipo = "")
+        {
+            if (tipo.Length == 0 || tipo == "Novo")
+            {
+                if (DBManager.conectado())
+                {
+                    try
+                    {
+                        string _sql = " INSERT INTO ROLO (ID,DESCRICAO,LARGURA) " +
+                                      " VALUES (@id,@descricao,@largura) ";
+
+                        cmd = new SQLiteCommand();
+                        cmd.Connection = DBManager._mainConnection;
+                        cmd.CommandText = _sql;
+
+                        string ultCodigo = Comandos.busca_campo("SELECT MAX(ID)+1 FROM ROLO");
+                        if (ultCodigo.Length == 0 || ultCodigo.Equals("0")) { ultCodigo = "1"; }
+
+                        cmd.Parameters.Add(new SQLiteParameter("@id", ultCodigo));
+                        cmd.Parameters.Add(new SQLiteParameter("@descricao", this.Descricao));
+                        cmd.Parameters.Add(new SQLiteParameter("@largura", this.Largura));
+
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Logs.Log(ex.Message);
+                    }
+                }
+            }
+            else if (tipo == "Atualizar")
+            {
+                if (DBManager.conectado())
+                {
+                    try
+                    {
+                        string _sql = " UPDATE ROLO SET " +
+                                      "      DESCRICAO = @descricao," +
+                                      "      LARGURA = @largura" +
+                                      " WHERE " +
+                                      "      Id = @id";
+
+                        cmd = new SQLiteCommand();
+                        cmd.Connection = DBManager._mainConnection;
+                        cmd.CommandText = _sql;
+
+                        cmd.Parameters.Add(new SQLiteParameter("@descricao", this.Descricao));
+                        cmd.Parameters.Add(new SQLiteParameter("@largura", this.Largura));
+
+                        cmd.Parameters.Add(new SQLiteParameter("@id", this.Id));
+
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Logs.Log(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GlassFilm/GlassFilm/frnCadRolo.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GlassFilm
+{
+    public partial class FrnCadRolo : Form
+    {
+        private Carregamentos carregamento;
+        private Controles controle;
+        private Rolo rolo;
+
+        private string linhaSelecionada = "0";
+        public bool iRotinaInterna;
+        public string codigoInterno;
+
+        public FrnCadRolo()
+        {
+            InitializeComponent();
+            carregamento = new Carregamentos();
+            controle = new Controles();
+        }
+
+        #region funções
+
+        private void carregaGridPrincipal()
+        {
+            string[] nomesColunas = { "Código", "Descrição", "Largura"};
+            int[] tamColunas = { 100, 300};
+            carregamento.carregarGrid(gridPrincipal, "  SELECT  " +
+                                                     "       ID," +
+                                                     "       DESCRICAO," +
+                                                     "       LARGURA" +
+                                                     "   FROM ROLO" +
+                                                     "       " +
+                                                     "   ORDER BY " +
+                                                     "       ID DESC"
+                                                     , tamColunas
+                                                     , nomesColunas);
+
+            tabRegistros.SelectedIndex = 0;
+        }
+
+        private void limpaCampos()
+        {
+            txtCodigo.Clear();
+            txtDescricao.Clear();
+            txtLargura.Clear();
+        }
+
+        private void controlePanels(string tipo)
+        {
+            pnlGrade.Enabled = false;
+            pnlManutencao.Enabled = false;
+            pnlCodigo.Enabled = false;
+
+            if (tipo == "Novo" || tipo == "Editar")
+            {
+                pnlManutencao.Enabled = true;
+            }
+            else
+            {
+                pnlGrade.Enabled = true;
+                pnlCodigo.Enabled = true;
+            }
+        }
+
+        #endregion
+
+        #region Form
+
+        private void Frncadrolo_Load(object sender, EventArgs e)
+        {
+            carregaGridPrincipal();
+        }
+
+        private void Minimizar(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Maximizar(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+                this.WindowState = FormWindowState.Maximized;
+            else
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Sair(object sender, EventArgs e)
+        {
+            this.Close();
+        }        
+
+        #endregion
+
+        #region Botoes
+
+        private void BotaoNovo(object sender, EventArgs e)
+        {
+            limpaCampos();
+            rolo = new Rolo();
+            controlePanels("Novo");
+            toolStatus.Text = "Novo";
+
+            controle.controlaBotoes(toolPrincipal, "Novo");
+            tabRegistros.SelectedIndex = 1;
+            txtDescricao.Focus();
+        }
+
+        private void BotaoEditar(object sender, EventArgs e)
+        {
+            toolStatus.Text = "Editando";
+
+            rolo = new Rolo();
+            foreach (DataRow linha in rolo.carrega(linhaSelecionada).Rows)
+            {
+                txtCodigo.Text = linhaSelecionada;
+                txtDescricao.Text = linha["DESCRICAO"].ToString();
+                txtLargura.Text = linha["LARGURA"].ToString();
+            }
+
+            controle.controlaBotoes(toolPrincipal, "Editar");
+            controlePanels("Editar");
+            tabRegistros.SelectedIndex = 1;
+        }
+
+        private void BotaoSalvar(object sender, EventArgs e)
+        {
+            #region Validações
+
+            if (txtDescricao.Text.Length == 0)
+            {
+                Mensagens.Informacao("Preencha o Campo Descrição");
+                txtDescricao.Focus();
+                return;
+            }
+
+            if (txtLargura.Text.Length == 0)
+            {
+                Mensagens.Informacao("Preencha o Campo Largura");
+                txtLargura.Focus();
+                return;
+            }
+
+            #endregion
+
+            rolo = new Rolo(txtDescricao.Text,txtLargura.Text);
+
+            if (toolStatus.Text == "Novo")
+            {
+                rolo.gravar("Novo");
+            }
+            else if (toolStatus.Text == "Editando")
+            {
+                //LogAlteracao.CriaLog(pnlManutencao, "", "Id", txtCodigo.Text);
+
+                rolo.Id = txtCodigo.Text;
+                rolo.gravar("Atualizar");
+            }
+
+            toolStatus.Text = "...";
+            controle.controlaBotoes(toolPrincipal, "Salvar");
+            limpaCampos();
+            controlePanels("Salvar");
+            carregaGridPrincipal();
+            tabRegistros.SelectedIndex = 0;
+
+            Mensagens.Informacao("Salvo com Sucesso!");
+        }
+
+        private void BotaoExcluir(object sender, EventArgs e)
+        {
+            rolo = new Rolo();
+            rolo.excluir(txtCodigo.Text);
+
+            controlePanels("Excluir");
+            limpaCampos();
+            controle.controlaBotoes(toolPrincipal, "Excluir");
+            toolStatus.Text = "...";
+            carregaGridPrincipal();
+            tabRegistros.SelectedIndex = 0;
+
+            Mensagens.Informacao("Excluído com Sucesso!");
+        }
+
+        private void BotaoCancelar(object sender, EventArgs e)
+        {
+            limpaCampos();
+            controlePanels("Cancelar");
+            controle.controlaBotoes(toolPrincipal, "Cancelar");
+            toolStatus.Text = "...";
+
+            tabRegistros.SelectedIndex = 0;
+        }
+
+        #endregion
+
+        #region Outras
+
+        private void GridCellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                txtFiltro.Text = gridPrincipal.Columns[gridPrincipal.CurrentCell.ColumnIndex].Name;
+                linhaSelecionada = gridPrincipal.Rows[e.RowIndex].Cells[0].FormattedValue.ToString();
+            }
+        }
+
+        private void GridCellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (iRotinaInterna)
+            {
+                codigoInterno = linhaSelecionada;
+                this.Close();
+            }
+            else
+            {
+                BotaoEditar(linhaSelecionada, EventArgs.Empty);
+            }
+        }
+
+        private void ConsultaAlteraTexto(object sender, EventArgs e)
+        {
+            if (txtFiltro.Text.Length > 0)
+                carregamento.MontaPesquisa(gridPrincipal, txtFiltro.Text, btnConsultar.Text);
+        }
+
+        private void BotaoLOG(object sender, EventArgs e)
+        {
+            List<string> lista = new List<string>();
+            lista.Add("");            
+        }
+
+        #endregion
+
+        private void btnCarregar_Click(object sender, EventArgs e)
+        {
+            if (txtCodigo.Text.Length > 0)
+            {
+                linhaSelecionada = txtCodigo.Text;
+                BotaoEditar(sender, e);
+            }
+        }
+
+        private void txtLargura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Validacoes.ValidaInternet.campoSomenteNumero(sender, e);
+        }
+
+    }
+}
